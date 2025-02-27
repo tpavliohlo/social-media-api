25e4f29dea2970501730ca57f2235431764b5558
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-02-27 17:23:18 +0100

    Create basic authentication for testing

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -133,3 +133,10 @@ AUTH_USER_MODEL = "user.User"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ]
+}
