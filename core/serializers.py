bdb6cc05f14cb94478264350f776a27dadd83726
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-02-27 17:37:33 +0100

    Create serializers for Post

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Profile
+from core.models import Profile, Post
 
 
 class RetrieveProfileSerializer(serializers.ModelSerializer):
@@ -19,3 +19,17 @@ class RetrieveProfileSerializer(serializers.ModelSerializer):
             'description',
             'privacy_settings',
         ]
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Post
+        fields = ["id", "title", "owner", "created_at"]
+
+
+class PostRetrieveSerializer(PostSerializer):
+    class Meta:
+        model = Post
+        fields = PostSerializer.Meta.fields + ["body"]
