0d4738e0acfb2e0866bb1938f3fdd32e8ab915c0
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-02-27 14:47:48 +0100

    Create admins for models and create model User

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Profile, Post, Comment, Follower, Like, Blocked
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Follower)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Blocked)
