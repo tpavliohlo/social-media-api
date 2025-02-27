779e415e93987640ec4445fd69ce5ac65ed33461
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-02-27 15:21:50 +0100

    Create urls

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path("api/v1/social-media/", include("core.urls"), name="social_media"),
+    path("api/v1/user", include("user.urls"), name="user"),
 ]
